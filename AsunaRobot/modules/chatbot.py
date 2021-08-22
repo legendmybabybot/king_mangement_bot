a2a4ce9560a23b4eb7f49b8d4d4f6bea09c55d97
rintarokabe <88771169+rintarokabe@users.noreply.github.com>
2021-08-22 16:34:54 +0530

    Update chatbot.py

--- AsunaRobot/modules/chatbot.py
+++ AsunaRobot/modules/chatbot.py
@@ -88,7 +88,7 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("Asuna AI Already Activated In This Chat")
+            await lel.edit("Brahma Bull AI Already Activated In This Chat")
             return
         await lel.edit(
             f"Brahma Bull AI Successfully Added For Users In The Chat {message.chat.id}"
