c8acb34b48680d0197228296dad579af47d382ac
rintarokabe <88771169+rintarokabe@users.noreply.github.com>
2021-11-10 11:22:13 +0530

    Update errors.py

--- AsunaRobot/utils/errors.py
+++ AsunaRobot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 from AsunaRobot import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001196083551)
+LOG_GROUP_ID = int(-1001523323615)
 
 def split_limits(text):
     if len(text) < 2048:
